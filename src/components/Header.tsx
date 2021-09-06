ddc375f506e07b7fc91bcffb5c4b0666413a1e78
Mauricio <maur.martins32@gmail.com>
2021-09-06 18:32:53 -0300

    Componente: Sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Flex, Text, Input, Icon} from "@chakra-ui/react";
-import {RiSearchLine} from "react-icons/ri";
+import {Flex, Text, Input, Icon, HStack, Box, Avatar} from "@chakra-ui/react";
+import {RiNotificationLine, RiSearchLine, RiUserAddLine} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -36,11 +36,40 @@ export function Header() {
           color="gray.500"
           variant="unstyled"
           placeholder="Buscar na plataforma"
-          _placeholder={{color: "gray.400"}}
+          _placeholder={{color: "gray.300"}}
           px="4"
           mr="4"
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Maurício Martins</Text>
+            <Text color="gray.300" fontSize="small">
+              maur.martins32@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Mauricio Martins"
+            src="https://github.com/maur32.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
