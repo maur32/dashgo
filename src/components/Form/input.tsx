0a1d6d2e2d41a60043906504d6339ea8285af35f
Mauricio <maur.martins32@gmail.com>
2021-09-13 18:31:18 -0300

    Revert "Validando os dados"
    This reverts commit e3a1e9bdc93321cd9b0151f86f4c1eb7634f1bb7.

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -3,24 +3,21 @@ import {
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
-  FormErrorMessage,
 } from "@chakra-ui/react";
 
 import {forwardRef, ForwardRefRenderFunction} from "react";
-import {FieldError} from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
-  error?: FieldError;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
-  {name, label, error = null, ...rest},
+  {name, label, ...rest},
   ref
 ) => {
   return (
-    <FormControl isInvalid={!!error}>
+    <FormControl>
       {!!label && <FormLabel htmlFor="email">{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -35,7 +32,6 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
         ref={ref}
         {...rest}
       />
-      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
 };
